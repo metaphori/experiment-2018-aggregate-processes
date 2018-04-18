5cac17613ac117e7635a4bfed723b916fff690b6
Danilo Pianini <danilo.pianini@unibo.it>
2018-04-18 17:34:37 +0200

    Fix RandomRoundTrip

--- src/main/scala/it/unibo/alchemist/model/implementations/actions/RandomRoundTrip.scala
+++ src/main/scala/it/unibo/alchemist/model/implementations/actions/RandomRoundTrip.scala
@@ -1,39 +1,56 @@
 package it.unibo.alchemist.model.implementations.actions
 
-import it.unibo.alchemist.model.implementations.movestrategies.routing.IgnoreStreets
 import it.unibo.alchemist.model.implementations.movestrategies.speed.ConstantSpeed
+import it.unibo.alchemist.model.implementations.routes.PolygonalChain
 import it.unibo.alchemist.model.interfaces._
 import it.unibo.alchemist.model.interfaces.movestrategies.TargetSelectionStrategy
 import it.unibo.alchemist.scala.PimpMyAlchemist._
 import org.apache.commons.math3.random.RandomGenerator
 import org.apache.commons.math3.util.FastMath.{atan2, cos, sin}
 
+private object RandomPosition {
+  private def randomInRange(rng: RandomGenerator, min: Double, max: Double) = rng.nextDouble() * (max - min) + min
+  def apply(env: Environment[_], rng: RandomGenerator, minX: Double, minY: Double, maxX: Double, maxY: Double): Position =
+    env.makePosition(randomInRange(rng, minX, maxX), randomInRange(rng, minY, maxY))
+}
+
 class RandomRoundTrip[T](environment: Environment[T], node: Node[T],
                          protected val reaction: Reaction[T],
                          rng: RandomGenerator,
-                         baseX: Double, baseY: Double,
+                         val base: Position,
                          minX: Double, minY: Double,
                          maxX: Double, maxY: Double,
                          waypointCount: Int,
                          speed: Double) extends AbstractConfigurableMoveNode(
     environment, node,
-    new IgnoreStreets,
+    (start, end) => base match {
+      case `end` => new PolygonalChain(base)
+      case `start` =>
+        val waypoints = (2 to waypointCount).map(_ => RandomPosition(environment, rng, minX, minY, maxX, maxY)).toList
+        new PolygonalChain[Position](start :: waypoints ::: end :: Nil :_*)
+      case _ => throw new IllegalStateException(s"Either the start ($start) or the end ($end) must match with the base ($base)")
+    },
     new TargetSelectionStrategy {
-      private var stack = List[Position]()
-      private val base = environment.makePosition(baseX, baseY)
-      private def randomInRange(min: Double, max: Double) = rng.nextDouble() * (maxX - minX) - minX
-      private def randomPos() = environment.makePosition(randomInRange(minX, maxX), randomInRange(minY, maxY))
+      var currentTarget = base
       override def getTarget: Position = {
-        if (stack.isEmpty) {
-          stack = base :: (1 to waypointCount).map(_ => randomPos()).toList ::: base :: Nil
+        currentTarget = environment.getPosition(node) match {
+          case `base` => RandomPosition(environment, rng, minX, minY, maxX, maxY)
+          case p if p == currentTarget => base
+          case _ => currentTarget
         }
-        val result = stack.head
-        stack = stack.tail
-        result
+        currentTarget
       }
     },
     new ConstantSpeed(reaction, speed)
   ) {
+
+  def this(environment: Environment[T], node: Node[T], reaction: Reaction[T], rng: RandomGenerator,
+    baseX: Double, baseY: Double,
+    minX: Double, minY: Double,
+    maxX: Double, maxY: Double,
+    waypointCount: Int,
+    speed: Double) = this(environment, node, reaction, rng, environment.makePosition(baseX, baseY), minX, minY, maxX, maxY, waypointCount, speed)
+
   override def getDestination(source: Position, target: Position, maxWalk: Double): Position =
     if (source.getDistanceTo(target) <= maxWalk) target
     else {
@@ -43,4 +60,5 @@ class RandomRoundTrip[T](environment: Environment[T], node: Node[T],
     }
 
   override def cloneAction(node: Node[T], reaction: Reaction[T]): Action[T] = ???
+
 }
